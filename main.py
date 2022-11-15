9b486a218e3fd1331d908c4e4320f59f2d09c2ba
Ramita <ramitameeriam@gmail.com>
2022-11-15 15:41:18 +0700

    copy code

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-print("Hello World")
+import tkinter as tk
+
+window = tk.Tk()
+window.title('Booking Nursery')
+window.minsize(width = 400, height = 400)
+
+title_label = tk.Label(master = window, text = "Desired Service")
+title_label.pack()
+
+window.mainloop()
