f7991123f4fbdaf693956a3488060d208c7e6c50
Eyeramita <118254982+Eyeramita@users.noreply.github.com>
2022-12-04 15:23:56 +0700

    Add files via upload

--- main.py
+++ main.py
@@ -1,10 +1,34 @@
-import tkinter as tk
+from tkinter import *
 
-window = tk.Tk()
-window.title('Booking Nursery')
-window.minsize(width = 400, height = 400)
+root = Tk()
 
-title_label = tk.Label(master = window, text = "Desired Service")
-title_label.pack()
+root.title('Booking Nursing-Home')
+root.geometry('300x200')#size's window
+root.configure(background="white")
+Label(root, text="Choose desired service", font=("Arial", 18), bg="white").pack()
 
-window.mainloop()
+def page():
+    meet = Tk()
+    meet.title('Booking Nursing-Home')
+    meet.geometry('800x600')
+    meet.configure(background="white")
+    Label(meet, text="Information", font=("Arial", 16), bg="white").grid(row=0, column=5)
+    Label(meet,text="Name", font=(18), bg="white").grid(row=1, column=1)
+    Label(meet,text="Age", font=(18), bg="white").grid(row=2, column=1)
+    Label(meet,text="Desease", font=(18), bg="white").grid(row=3, column=1)
+    Label(meet,text="Bedridden", font=(18), bg="white").grid(row=4, column=1)
+    Label(meet,text="Neck piercing", font=(18), bg="white").grid(row=5, column=1)
+
+
+Var1 = IntVar()
+A1 = Checkbutton(root, text="Shelter the Center", variable=Var1, font=(18), bg="white", command=page)
+A1.pack(anchor=W, padx=10, pady=10)
+Var2 = IntVar()
+A2 = Checkbutton(root, text="Send Staff to the patient's home", variable=Var2, font=(18), bg="white")
+A2.pack(anchor=W, padx=10, pady=10)
+
+
+
+root.mainloop()
+
+ 
